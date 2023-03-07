53b4642e87b143a6d7e74ba4a70582c2eca0bbbe
developer1 <developer1@gmail.com>
2023-03-07 13:57:14 +0100

    commit chngs

---
+++ src/main/java/com/kani/cd002/App.java
@@ -0,0 +1,13 @@
+package com.kani.cd002;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Bonjour les amis" );
+    }
+}
